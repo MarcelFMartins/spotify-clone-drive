da42c119b7296ed3f615b78a22477a7b6caeadb0
Marcel <doiscontroles25@gmail.com>
2025-07-04 14:59:57 -0300

    Fix: Redirect unauthenticated users to login

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { getSession } from 'next-auth/react';
 import { google } from 'googleapis';
 import { getToken } from '../utils/auth';
 import { listFolders } from '../utils/drive';
@@ -5,26 +6,20 @@ import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin',
+        permanent: false,
+      },
+    };
+  }
+
   const auth = await getToken(context.req);
   const drive = google.drive({ version: 'v3', auth });
   const folders = await listFolders(drive);
 
   return { props: { folders } };
-}
-
-export default function Home({ folders }) {
-  return (
-    <div className={styles.container}>
-      <h1>Escolha um Gênero</h1>
-      <ul>
-        {folders.map(folder => (
-          <li key={folder.id}>
-            <Link href={`/folder/${folder.id}`} legacyBehavior>
-              <a>{folder.name}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 }
