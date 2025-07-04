ba35094d211ebd7ce24e55714311854f0360ab65
Marcel <doiscontroles25@gmail.com>
2025-07-04 15:09:38 -0300

    Sua mensagem aqui explicando a alteração

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,23 @@ import { listFolders } from '../utils/drive';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
+export default function Home({ folders }) {
+  return (
+    <div className={styles.container}>
+      <h1>Pastas do Google Drive</h1>
+      <ul>
+        {folders.map((folder) => (
+          <li key={folder.id}>
+            <Link href={`/folder/${folder.id}`}>
+              <a>{folder.name}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export async function getServerSideProps(context) {
   const session = await getSession(context);
 
